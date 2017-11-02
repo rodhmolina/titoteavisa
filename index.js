0df3e6b0a6c95bd8900dccba1a66702dddfe185d
Rodrigo Molina <rodhmolina@gmail.com>
2017-11-01 21:07:52 -0300

    working with time and date for schedules

--- index.js
+++ index.js
@@ -623,23 +623,25 @@ app.post('/fulfillment/', (req, res) => {
                 app.aiSessions.set(sessionId,sender);
 				console.log("saving " + JSON.stringify(sessionId) + ':' + JSON.stringify(sender));
             }
-		console.log("calculating time for: " + data.result.parameters.time);
+		console.log("calculating time for: " + data.result.parameters.date + ' ' + data.result.parameters.time);
 		var date = data.result.parameters.date;
 		var time = data.result.parameters.time;
-		
+		console.log(date.toString());
 		
 		if (!date) date = new Date();
 		else date = new Date(date);
+		console.log(date.toString());
 		
 		if(!time) time = Date.now().getTime;
 		date.setTime(time);
-				
+		console.log(date.toString());
+		
 		var milliseconds = date - Date.now();
 		
 		setTimeout(function(){
 			facebookBot.doTextResponse(sender, "evento automatico");
 		}, 3000);
-		console.log("timer on: " + milliseconds);
+		console.log("timer on: " + milliseconds.toString());
 	}
 	console.log("fulfillment:\n" + JSON.stringify(data));
     res.send("ok");
